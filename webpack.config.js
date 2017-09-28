5160ee942bd7f31680a25b3e7ebdbb1be7df4659
Jeong Lee <jsl133@ucsd.edu>
2017-09-28 08:01:09 -0700

    hi

--- webpack.config.js
+++ webpack.config.js
@@ -9,9 +9,22 @@ module.exports = {
     path: __dirname + "/js",
     filename: "bundle.js"
   },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+          }
+        }
+       ]
+     },
   plugins: debug ? [] : [
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
   ],
+  target: 'node'
 };
