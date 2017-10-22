47920b0d785c820c4659ba09f250f402dd5b25cc
Jeong Lee <jsl133@ucsd.edu>
2017-10-22 16:11:52 -0700

    wtf

--- app.js
+++ app.js
@@ -2,9 +2,10 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var apath = path.dirname(require.main.filename)
 
 app.get('/', function (req, res) {
-  res.sendFile('index.html',{root: './public'});
+  res.sendFile('/public/index.html',{root: apath});
 })
 app.use(express.static(__dirname + '/public'));
 app.listen(process.env.PORT || 5000);
