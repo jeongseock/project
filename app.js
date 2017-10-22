480f1f5d70c0135422cd133c9e1673a1432ab8d6
Jeong Lee <jsl133@ucsd.edu>
2017-10-22 15:36:00 -0700

    yes

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 // set up server
 var express = require('express');
 var app = express();
-var port = 8080;
 
 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/public/index.html');
+  res.sendFile(__dirname +'./public/index.html');
 })
 app.use(express.static(__dirname + '/public'));
-app.listen(process.env.PORT || port);
-console.log("App listening on port " + port);
+app.listen(process.env.PORT || 5000);
+console.log("App listening on port " + 5000);
