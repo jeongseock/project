160ad84ad6336bc0f4f3a2df8c852981d05db2ac
Jeong Lee <jsl133@ucsd.edu>
2017-10-22 15:55:19 -0700

    added path

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 // set up server
 var express = require('express');
 var app = express();
-
+var path = require('path');
 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname +'./public/index.html');
+  res.sendFile(path.resolve(__dirname +'/public/index.html'));
 })
 app.use(express.static(__dirname + '/public'));
 app.listen(process.env.PORT || 5000);
