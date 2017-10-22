155294dc369513e60012dffe92d5b009c004d672
Jeong Lee <jsl133@ucsd.edu>
2017-10-22 14:19:02 -0700

    changing listener

--- app.js
+++ app.js
@@ -8,5 +8,5 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/public/index.html');
 })
 app.use(express.static(__dirname + '/public'));
-app.listen(port);
+app.listen(process.env.PORT || port);
 console.log("App listening on port " + port);
